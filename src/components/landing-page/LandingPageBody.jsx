0b67292d9563dbabe8dfe0c749b8b0f3592038e6
marsian8 <spanarve@gmail.com>
2022-12-14 23:19:02 +0530

    changes

--- src/components/landing-page/LandingPageBody.jsx
+++ src/components/landing-page/LandingPageBody.jsx
@@ -7,14 +7,14 @@ import { useEffect } from "react";
 const topCardVariants = {
   visible: {
     opacity: 1,
-    scale: 0,
+    scale: 1,
     transition: {
       duration: 0.5,
     },
   },
   hidden: {
     opacity: 0,
-    scale: 1,
+    scale: 0,
   },
 };
 
