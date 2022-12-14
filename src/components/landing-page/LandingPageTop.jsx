d970f811140c40f46b7bdc0c33e702dd0299d3cf
marsian8 <spanarve@gmail.com>
2022-12-14 21:36:10 +0530

    changes

--- src/components/landing-page/LandingPageTop.jsx
+++ src/components/landing-page/LandingPageTop.jsx
@@ -1,60 +1,67 @@
-import React from 'react'
-import './LandingPageTop.scoped.css'
+import React from "react";
+import "./LandingPageTop.scoped.css";
 // eslint-disable-next-line
-import { TypeAnimation } from 'react-type-animation';
-import 'font-awesome/css/font-awesome.min.css';
-         
+import { TypeAnimation } from "react-type-animation";
+// import 'font-awesome/css/font-awesome.min.css';
 
 const LandingPageTop = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <div className="container-left">
-            <div className='container-left-heading'>
-              Healthy <br/> <span>food</span> for <br/>a healthy life
-            </div>
-            <div className='container-left-content'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores eius, impedit aliquid amet atque assumenda minus dignissimos omnis sit voluptatibus vero! Modi qui dolore quisquam!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nisi, numquam ullam cumque perferendis facere quasi nostrum molestias repellat totam non soluta! Adipisci, alias esse!
-            </div>
-            <div className='container-left-buttons'>
-                <button>
-                    Login
-                </button>
-                <button>
-                    SignUp
-                </button>
-            </div>
+        <div className="container-left-heading">
+          Healthy <br /> <span>food</span> for <br />a healthy life
         </div>
-        <div className='container-right'>
-          <div className='container-right-circle'>
-          </div>
-          <img src={require('../../assets/landingpage-mascot.png')} alt='moscot'/>
-
-          <div className='animated-area'>
-          <div className='animated-text-area'>
-
-          <TypeAnimation
-      sequence={[
-        'I Ate 3 slice of white-bread',
-        1500,
-        '2 Glasses of milk',
-        2000,
-        '1 Chicken leg',
-        1000,
-        'You can Also Upload a picture',
-        3000
-      ]}
-      wrapper="div"
-      cursor={true}
-      repeat={Infinity}
-      style={{ fontSize: '2.2em', opacity: '80%' }}
-      />
+        <div className="container-left-content">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
+          eius, impedit aliquid amet atque assumenda minus dignissimos omnis sit
+          voluptatibus vero! Modi qui dolore quisquam! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Accusamus nisi, numquam ullam
+          cumque perferendis facere quasi nostrum molestias repellat totam non
+          soluta! Adipisci, alias esse!
+        </div>
+        <div className="container-left-buttons">
+          <button>Login</button>
+          <button>SignUp</button>
+        </div>
       </div>
+      <div className="container-right">
+        <div className="container-right-circle" />
+        <img
+          src={require("../../assets/landingpage-mascot.png")}
+          alt="moscot"
+        />
+
+        <div className="animated-area">
+          <div className="animated-text-area">
+            <TypeAnimation
+              sequence={[
+                "Tell us what you ate",
+                5000,
+                "I ate 3 slices of white-bread",
+                1500,
+                "2 Glasses of milk",
+                2000,
+                "I had a chicken leg",
+                1000,
+                "Or just upload a picture",
+                3000,
+                `Leave the rest to us`,
+                1500,
+                `We'll track your calories`,
+                1500,
+                "And Nutrients",
+                1000
+              ]}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: "2.2em", opacity: "80%" }}
+            />
+          </div>
+        </div>
       </div>
+    </div>
+  );
+};
 
-          </div>
-          </div>
-  )
-}
-
-export default LandingPageTop
+export default LandingPageTop;
