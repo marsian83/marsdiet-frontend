7e14a06ff1463d9791dd052319f6f802c22416fd
marsian8 <spanarve@gmail.com>
2022-12-15 00:10:27 +0530

    changes

--- src/components/landing-page/LandingPageBody.jsx
+++ src/components/landing-page/LandingPageBody.jsx
@@ -45,12 +45,39 @@ export const LandingPageBody = () => {
         <div className="top-cards-container">
           <div className="top-card" id="top-card1">
             <img src={require("../../assets/jogging.png")} alt="" />
+            <div className="top-card-top" />
+            <div className="top-card-bottom">
+              <h4>Take healthy choices</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Explicabo recusandae consequatur minima aliquam odit repudiandae
+                hic?
+              </p>
+            </div>
           </div>
           <div className="top-card" id="top-card2">
             <img src={require("../../assets/drinking-water.png")} alt="" />
+            <div className="top-card-top" />
+            <div className="top-card-bottom">
+              <h4>Take healthy choices</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Explicabo recusandae consequatur minima aliquam odit repudiandae
+                hic?
+              </p>
+            </div>
           </div>
           <div className="top-card" id="top-card3">
             <img src={require("../../assets/sleep-analysis.png")} alt="" />
+            <div className="top-card-top" />
+            <div className="top-card-bottom">
+              <h4>Take healthy choices</h4>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Explicabo recusandae consequatur minima aliquam odit repudiandae
+                hic?
+              </p>
+            </div>
           </div>
         </div>
       </motion.div>
