85baa938c8e0d265678c5966a5d455b798c9f579
marsian8 <spanarve@gmail.com>
2022-12-15 14:37:19 +0530

    changes

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -9,12 +9,12 @@ const Signup = (props) => {
         <p>Hey, Enter your details to get sign in to your account.</p>
         <div>
           <div className="input-bar">
-            <label for="name">username</label>
+            <label htmlFor="name">username</label>
             <input type="text" id="name" className="input" />
             <box-icon name="user" />
           </div>
           <div className="input-bar">
-            <label for="password">password</label>
+            <label htmlFor="password">password</label>
             <input type="password" id="password" className="input" />
             <box-icon name="lock-alt" />
           </div>
